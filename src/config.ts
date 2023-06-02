01862dd77efb71c24be62a0f437f8cfdf12aa43c
ghaedi1993 <ghaedi1993@gmail.com>
2023-06-02 21:22:48 +0330

    add new env var

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,20 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
-
-const checkEnv = (envVar: string, defaultValue?: string) => {
+type DefaultValue = string | boolean; 
+const checkEnv = (envVar: string, defaultValue?: DefaultValue) => {
   if (!process.env[envVar]) {
-    if (defaultValue) {
+    if (defaultValue !== undefined) {
       return defaultValue;
     }
     throw new Error(`Please define the Enviroment variable"${envVar}"`);
-  } else {
+  } else { 
     return process.env[envVar] as string;
   }
 };
-export const PORT: number = parseInt(checkEnv('PORT'), 10);
-export const DBURL: string = checkEnv('DBURL');
-export const DBURL_TEST: string = checkEnv('DBURL_TEST');
+export const PORT: number = parseInt(checkEnv('PORT') as string, 10);
+export const DBURL: string = checkEnv('DBURL') as string;
+export const DBURL_TEST: string = checkEnv('DBURL_TEST') as string;
 export const CORS_ORIGINS = ['http://localhost:3000'];
-export const NODE_ENV: string = checkEnv('NODE_ENV', 'development');
+export const NODE_ENV: string = checkEnv('NODE_ENV', 'development') as string;
+export const INSIDE_DOCKER_CONTAINER:boolean = checkEnv('INSIDE_DOCKER_CONTAINER',false) as boolean;
