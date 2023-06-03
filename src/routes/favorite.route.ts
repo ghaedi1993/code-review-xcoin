3a6f26bc05a1cb180e35c709ac2545bb3ed8fcbc
ghaedi1993 <ghaedi1993@gmail.com>
2023-06-03 18:32:38 +0330

    add joi for validation

--- src/routes/favorite.route.ts
+++ src/routes/favorite.route.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import { favoriteService } from '../services';
+import Joi from 'joi';
+import { isValidObjectId } from 'mongoose';
 const router = Router();
 
 router.get('/', async (req, res, next) => {
@@ -12,6 +14,20 @@ router.get('/', async (req, res, next) => {
 });
 
 router.get('/:profile_id', async (req, res, next) => {
+  const schema = Joi.object({
+    profile_id: Joi.string().custom((value, helpers) => {
+      if (!isValidObjectId(value)) {
+        return helpers.error('any.invalid');
+      }
+      return value;
+    }, 'Object Id Validation'),
+  });
+
+  const { error } = schema.validate(req.params);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+
   const profileId = req.params.profile_id;
 
   try {
--- src/routes/simulator.route.ts
+++ src/routes/simulator.route.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import { simulatorService } from '../services';
+import Joi from 'joi';
+import { isValidObjectId } from 'mongoose';
 const router = Router();
 
 router.get('/', async (req, res, next) => {
@@ -25,11 +27,25 @@ router.get('/:profile_id', async (req, res, next) => {
 });
 
 router.post('/:profile_id', async (req, res, next) => {
+  const schema = Joi.object({
+    profile_id: Joi.string().custom((value, helpers) => {
+      if (!isValidObjectId(value)) {
+        return helpers.error('any.invalid');
+      }
+      return value;
+    }, 'Object Id Validation'),
+  });
+
+  const { error } = schema.validate(req.params);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+
   const profile_id = req.params.profile_id;
   const simulatorDto = {
     profile_id,
-    ...req.body
-  }
+    ...req.body,
+  };
   try {
     const simulator = await simulatorService.createSimulator(simulatorDto);
     res.status(201).json(simulator);
